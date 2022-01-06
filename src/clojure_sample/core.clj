6fd1a55315f0abeef6f2593ccccd0992300e686e
Marina Cavalari <marina.cavalari.mc@gmail.com>
2022-01-06 17:09:13 -0300

    add examples

--- src/clojure_sample/core.clj
+++ src/clojure_sample/core.clj
@@ -1,8 +1,29 @@
 (ns clojure-sample.core
   (:require [databricks-sdk.core :as sdk]))
 
-(comment )
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(comment
+  (sdk/create-cluster! {:token (System/getenv "DATABRICKS_STAGING_TOKEN")
+                       :timeout 30000
+                       :host host-staging
+                       :body {:cluster_name "my-cluster"
+                              :spark_version "7.3.x-scala2.12"
+                              :node_type_id "i3.xlarge"
+                              :spark_conf {:spark.speculation true}
+                              :aws_attributes {:availability "SPOT"
+                                               :zone_id  "us-west-2a"}
+                              :num_workers 25}})
+  
+  (sdk/list-clusters {:token (System/getenv "DATABRICKS_STAGING_TOKEN")
+                      :timeout 30000
+                      :host host-staging
+                      :body {}})
+  
+  (sdk/terminate-cluster! {:token (System/getenv "DATABRICKS_STAGING_TOKEN")
+                      :timeout 30000
+                      :host host-staging
+                      :body {:cluster_id "1001-200146-blimp4"}})
+  
+  (sdk/start-cluster! {:token (System/getenv "DATABRICKS_STAGING_TOKEN")
+                           :timeout 30000
+                           :host host-staging
+                           :body {:cluster_id "1001-200146-blimp4"}}))
